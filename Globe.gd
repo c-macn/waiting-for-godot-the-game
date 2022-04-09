40527717ed986f48bbaa9cfdf6e25e06f5c8bf1f
Chris McNulty <christopher.mcnulty891@gmail.com>
2022-04-09 13:53:44 +0100

    Control globe rotation via keyboard input

--- Globe.gd
+++ Globe.gd
@@ -1,6 +1,12 @@
 extends Sprite
 class_name Globe
 
-func _process(_delta: float) -> void:
-	rotation_degrees -= 0.1
+
+func _input(_event: InputEvent) -> void:
+	if Input.is_action_pressed("walk_left") and not Input.is_action_pressed("walk_right"):
+		var updated_degrees = rotation_degrees + 0.6
+		rotation_degrees = updated_degrees
 	
+	if Input.is_action_pressed("walk_right") and not Input.is_action_pressed("walk_left"):
+		var updated_degress = rotation_degrees - 0.6
+		rotation_degrees = updated_degress
